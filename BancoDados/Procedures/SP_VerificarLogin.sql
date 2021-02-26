2bec1b50a4bd6e67e2beb2dcedde321ee3bd0d02
IankoFCR <70598532+IankoFCR@users.noreply.github.com>
2021-02-26 11:00:49 -0300

    SPRINT00x-2021: atualizando proc de verificar login

--- BancoDados/Procedures/SP_VerificarLogin.sql
+++ BancoDados/Procedures/SP_VerificarLogin.sql
@@ -1,23 +1,25 @@
  IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'[dbo].[SP_VerificarLogin]') AND objectproperty(id, N'IsPROCEDURE')=1)
-	DROP PROCEDURE [dbo].[SP_VerificarLogin]
+    DROP PROCEDURE [dbo].[SP_VerificarLogin]
 GO
 CREATE PROCEDURE [dbo].[SP_VerificarLogin]
     @Email VARCHAR(254)
 
 AS
-	/* 
-	Documentação
-	Módulo............: Usuario, Email
-	Objetivo..........: Consultar o login de acordo com a coluna Usuario.Senha e Email.EnderecoEmail
-	EX................: EXEC [dbo].[SP_VericarLogin] 'joaozin@email.co'
-	*/
-	BEGIN
-		SELECT e.EnderecoEmail, 
-				u.Senha,
-				u.EGestor
-			FROM Email AS e
-				INNER JOIN [dbo].[Usuario] AS u WITH(NOLOCK)
-					ON e.IdUsuario = u.IdUsuario
-			WHERE e.EnderecoEmail = @Email
+    /* 
+    DocumentaÃ§Ã£o
+    MÃ³dulo............: Usuario, Email
+    Objetivo..........: Consultar o login de acordo com a coluna Usuario.Senha e Email.EnderecoEmail
+    EX................: EXEC [dbo].[SP_VerificarLogin] 'dsg@gsd.cda'
+    */
+    BEGIN
+        SELECT    u.IdUsuario,
+                e.EnderecoEmail, 
+                u.Senha,
+                u.EGestor,
+                u.EstaAtivo
+            FROM Email AS e
+                INNER JOIN [dbo].[Usuario] AS u WITH(NOLOCK)
+                    ON e.IdUsuario = u.IdUsuario
+            WHERE e.EnderecoEmail = @Email
 
-	END
+    END
