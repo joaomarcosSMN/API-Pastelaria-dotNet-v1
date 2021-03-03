bf2107aee124d77098caa6b12f428aaa7a3f0919
Joao Marcos <joao.marcos@smn.com.br>
2021-03-03 14:57:27 -0300

    SPRINT00x-2021: conexxao

--- PastelariaSMN/Infra/Connection.cs
+++ PastelariaSMN/Infra/Connection.cs
@@ -11,8 +11,8 @@ namespace PastelariaSMN.Infra
 
         public Connection(IConfiguration configuration)
         {
-            //connection = new SqlConnection(configuration.GetValue<string>("ConnectionStrings:DefaultConnection"));
-            connection = new SqlConnection(configuration.GetValue<string>("ConnectionStrings:AlternativeConnection"));
+            connection = new SqlConnection(configuration.GetValue<string>("ConnectionStrings:DefaultConnection"));
+            // connection = new SqlConnection(configuration.GetValue<string>("ConnectionStrings:AlternativeConnection"));
             // connection = new SqlConnection(configuration.GetValue<string>("ConnectionStrings:AlternativeConnection2"));
             // connection = new SqlConnection(configuration.GetValue<string>("ConnectionStrings:AlternativeConnection3"));
         }
